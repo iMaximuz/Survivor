41636f0db1ef4b75c1827345ae88b74dbd71cee4
Vanessa Soto <vane.97x3@hotmail.com>
2018-04-12 04:41:27 -0500

    index terminado

--- js/index.js
+++ js/index.js
@@ -2,33 +2,49 @@ function start(){
     // Get the modal
     let modals = [];
     var settingsModal = $('#settingsModal');
+    var highscoresModal = $('#highscoresModal');
+    var pauseModal = $('#pauseModal');
+    
     modals.push(settingsModal);
+    modals.push(highscoresModal);
+    modals.push(pauseModal);
 
     // Get the button that opens the modal
-    $("#settings").click(function(){
-        settingsModal
-        modal.style.display = "block";
+    $("#settings").click(function() {
+        showModal(settingsModal);
+    });
+
+    $("#highScores").click(function() {
+        showModal(highscoresModal);
     });
 
     // Get the <span> element that closes the modal
-    var span = $(".close");
-
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+    $(".close").click(function(e) {
+        let self = $(this);
+        hideModal(self.parents('.modal'));
+    });
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        for(let modal of modals){
+            if (event.target == modal[0]) {
+                hideModal(modal)
+            }
         }
     } 
+
+    function showModal(modal) {
+        modal.addClass('show');
+    }
+
+    function hideModal(modal) {
+        modal.removeClass('show');
+    }
+
+    window.onkeydown = function(e){
+        if(e.keyCode == 27)
+            showModal(pauseModal)
+    };
 }
 
 $(document).ready(start);
