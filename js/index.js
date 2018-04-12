330fcff94089c28c798697128599454d6e69673e
Vanessa Soto <vane.97x3@hotmail.com>
2018-04-12 03:33:53 -0500

    Index styles

---
+++ js/index.js
@@ -0,0 +1,34 @@
+function start(){
+    // Get the modal
+    let modals = [];
+    var settingsModal = $('#settingsModal');
+    modals.push(settingsModal);
+
+    // Get the button that opens the modal
+    $("#settings").click(function(){
+        settingsModal
+        modal.style.display = "block";
+    });
+
+    // Get the <span> element that closes the modal
+    var span = $(".close");
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    } 
+}
+
+$(document).ready(start);
